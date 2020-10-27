99f607a31b43aac7b6912879513ae32595d7fd86
ManasaGB1998 <72864615+ManasaGB1998@users.noreply.github.com>
2020-10-27 16:06:55 +0530

    Add files via upload

---
+++ 26-10-2020/classAssigment.swift
@@ -0,0 +1,38 @@
+Class MovieBookingCounter {
+	var movieName
+	var language
+	var seatsAvailable
+	var totalSeats
+	
+	func ticketIssueing()
+	func hallOccupancyStatus()    //full, empty?
+	func availableSeatStatus()
+	func costOfCategory()         //gold,platinum,silver...
+}
+
+Class RestaurantMenu {
+	let hotelName
+	let location
+	let foodType  				  //chinese,italian,northInsian, southIndian...
+	
+	func categories				  //starters,meals,dishes,icecreams,juice...
+	func listOfItemsInEachCategory
+	func rateItems
+}
+
+Class Car {
+	var carName
+	var carModel
+	var carManufacturer
+	var price
+	
+	//func suggestCarBasedOnRequirements()
+	//func verifyCustomerIdentity()
+	//func checkAvailabeCars
+	
+	func checkSpeed()
+	func indtructions()            //seatBelt,light...
+	func map()                     //
+	func isLocked()
+}
+	
